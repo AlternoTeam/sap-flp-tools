121550389da57d85571dbf587d3248cf00f0d74f
gal tager <gal@alterno.io>
2023-06-19 12:56:59 +0300

    d

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+export default class LaunchpadUtils {
+    static crossAppNavigate(sSemObject: string, sAction: string, oParams: object, sAppend?: string): void;
+}
+declare const crossAppNavigate: typeof LaunchpadUtils.crossAppNavigate;
+export { crossAppNavigate };
