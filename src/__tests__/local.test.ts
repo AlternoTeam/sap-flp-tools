e39965697a763f78667af65c3d716e53911d5874
gal tager <gal@alterno.io>
2023-06-19 19:53:54 +0300

    check version update

--- src/__tests__/local.test.ts
+++ src/__tests__/local.test.ts
@@ -1,11 +1,10 @@
 import { crossAppNavigate } from '../index';
 import LfpUtils from '../index';
 
-
 test('My crossAppNavigate with destract', () => {
-    expect(crossAppNavigate("d", "dd", {}, "dd"))
+  expect(crossAppNavigate('d', 'dd', {}, 'dd'));
 });
 
 test('My crossAppNavigate with default export', () => {
-    expect(LfpUtils.crossAppNavigate("d", "dd", {}, "dd"))
+  expect(LfpUtils.crossAppNavigate('d', 'dd', {}, 'dd'));
 });
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,48 @@
-
 export default class LaunchpadUtils {
-    public static crossAppNavigate(sSemObject: string, sAction: string, oParams: object, sAppend?: string) {
-        if (typeof window !== 'undefined' && window.hasOwnProperty('sap')) {
-            // @ts-ignore
-            sap.ushell.Container.getServiceAsync("CrossApplicationNavigation").then(function(oService){
-                const hash =
-                (oService &&
-                    oService.hrefForExternal({
-                        target: {
-                            semanticObject: sSemObject,
-                            action: sAppend ? sAction + sAppend : sAction,
-                        },
-                        params: oParams,
-                    })) || "";
-                const sintent = "#" + sSemObject + "=" + sAction;
+  public static crossAppNavigate(sSemObject: string, sAction: string, oParams: object, sAppend?: string) {
+    if (typeof window !== 'undefined' && window.hasOwnProperty('sap')) {
+      // @ts-ignore
+      sap.ushell.Container.getServiceAsync('CrossApplicationNavigation').then(
+        function (oService) {
+          const hash =
+            (oService &&
+              oService.hrefForExternal({
+                target: {
+                  semanticObject: sSemObject,
+                  action: sAppend ? sAction + sAppend : sAction,
+                },
+                params: oParams,
+              })) ||
+            '';
+          const sintent = '#' + sSemObject + '=' + sAction;
 
-                oService.isIntentSupported([sintent]).done(
-                    function (olntentSupported: object) {
-                        // @ts-ignore
-                        if (
-                            olntentSupported &&
-                            // @ts-ignore
-                            olntentSupported[sintent] &&
-                            // @ts-ignore
-                            olntentSupported[sintent].supported === true
-                        ) {
-                            oService.toExternal({
-                                target: {
-                                    shellHash: hash,
-                                },
-                            });
-                        } else {
-                            console.error("Intent " + sintent + " is not supported");
-                        }
-                    // @ts-ignore
-                    }.bind(this));
-                }.bind(this));
-        }
-        else {
-            console.log("You must work in fiori launchpad")
-        }
+          oService.isIntentSupported([sintent]).done(
+            function (olntentSupported: object) {
+              // @ts-ignore
+              if (
+                olntentSupported &&
+                // @ts-ignore
+                olntentSupported[sintent] &&
+                // @ts-ignore
+                olntentSupported[sintent].supported === true
+              ) {
+                oService.toExternal({
+                  target: {
+                    shellHash: hash,
+                  },
+                });
+              } else {
+                console.error('Intent ' + sintent + ' is not supported');
+              }
+              // @ts-ignore
+            }.bind(this),
+          );
+        }.bind(this),
+      );
+    } else {
+      console.log('You must work in fiori launchpad');
     }
+  }
 }
-const crossAppNavigate = LaunchpadUtils.crossAppNavigate
-export { crossAppNavigate }
+const crossAppNavigate = LaunchpadUtils.crossAppNavigate;
+export { crossAppNavigate };
