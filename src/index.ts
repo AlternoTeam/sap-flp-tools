3c7402b83e44263844025c32ef9ee87b4e9c47fc
AlternoTeam <136958140+AlternoTeam@users.noreply.github.com>
2023-06-20 09:44:22 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,9 @@ export default class LaunchpadUtils {
                   action: sAppend ? sAction + sAppend : sAction,
                 },
                 params: oParams,
-              }) ||
+              }).replace("?sap-app-origin-hint=", "")) ||
             '';
           
-          hash = hash..replace("?sap-app-origin-hint=", "");
           const sintent = '#' + sSemObject + '-' + sAction;
 
           oService.isIntentSupported([sintent]).done(
